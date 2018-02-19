c59999608f3822ad699f56e28dddbd2117ac979c
Ben Purcell <benjapurcell@gmail.com>
2018-02-19 14:57:46 -0500

    use the parser

--- mc.ml
+++ mc.ml
@@ -8,18 +8,14 @@ let add_input file = in_files := file :: !in_files
 
 let compile file in_chan = begin
   let lexbuf = Lexing.from_channel in_chan in
+  let _ = Parser.prog Lexer.token  lexbuf in
   ()
-end
-
-let compile_file file = begin
-  let in_chan = open_in file in
-  compile file in_chan
 end
 
 let () = begin
   Arg.parse opts add_input usage;
   match List.rev !in_files with
   | [] -> compile "<stdin>" stdin
-  | files -> List.iter compile_file files;
+  | files -> List.iter (fun file -> compile file (open_in file)) files;
   exit 0;
 end
